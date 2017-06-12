ea36bfd43b14fa47fe98b3b2004b9121c42a4fef
dcheley <danielcheley@hotmail.com>
2017-06-12 13:56:27 -0400

    Creating test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([{ email: "email@one.com", password: "123456"},
+            { email: "email@two.com", password: "123456"},
+            { email: "email@three.com", password: "123456"},
+            { email: "email@four.com", password: "123456"},
+            { email: "email@five.com", password: "123456"}])
