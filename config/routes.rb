312215ae80f76a19d2fc8a98975ad63bbcac6070
dcheley <danielcheley@hotmail.com>
2017-06-13 14:35:28 -0400

    Removing registrations#new route with Devise helper

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
     root to: 'users#index'
   end
 
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+      put 'users' => 'devise/registrations#update', as: 'user_registration'
+  end
 
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show]
 end
