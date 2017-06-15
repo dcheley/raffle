3c2faf47e9318e70d56c6e85a63339eb575d0885
dcheley <danielcheley@hotmail.com>
2017-06-15 11:10:29 -0400

    Adding new and edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index, :show]
-  resources :transactions, only: [:index, :create, :update, :destroy]
+  resources :transactions, only: [:index, :new, :create, :edit, :update, :destroy]
 end
