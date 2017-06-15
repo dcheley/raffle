a533cb2f32497e34ebd99b30fdb686aaada1b323
dcheley <danielcheley@hotmail.com>
2017-06-15 11:09:33 -0400

    Removing react-rails gem related files

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 
-gem 'react-rails'
 # Use SCSS for stylesheets
 gem 'bootstrap-sass', '~> 3.3.6'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
 //= require jquery
 //= require rails-ujs
 //= require turbolinks
-//= require react
-//= require react_ujs
-//= require components
 //= require_tree .
 //= require bootstrap
