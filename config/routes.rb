3dc9f481f5bba417915d2d16e8510cd41c5d3e5f
dcheley <danielcheley@hotmail.com>
2017-06-15 09:55:10 -0400

    Removing new & edit links, adding update

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index, :show]
-  resources :transactions, only: [:index, :new, :create, :edit]
+  resources :transactions, only: [:index, :create, :update]
 end
