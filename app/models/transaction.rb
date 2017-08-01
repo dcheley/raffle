3af35de34f633e8563ed4c35e519a6e96f663e18
dcheley <danielcheley@hotmail.com>
2017-08-01 12:05:24 -0400

    Updating quantity validation & error message locales for UX. Also adjusting view page text

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,6 @@ class Transaction < ApplicationRecord
 
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => [:create, :update] }
   validates :payee, presence: true
-  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
   validates :debt, numericality: { greater_than_or_equal_to: 0 }
 end
