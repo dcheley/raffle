5fcf2a191c5a4a9d4297b72fbe696747c790883b
dcheley <danielcheley@hotmail.com>
2017-08-31 12:14:34 -0400

    Adjusting values displayed on /users, initializing devise confirmable module, changing devise email variables & styling devise forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable,
-         :recoverable, :rememberable, :registerable, :trackable, :validatable
+         :recoverable, :rememberable, :registerable, :trackable, :validatable, :confirmable
 
   has_many :transactions
   has_many :tickets, through: :transactions
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830194816) do
+ActiveRecord::Schema.define(version: 20170831143439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 20170830194816) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.boolean "admin", default: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
