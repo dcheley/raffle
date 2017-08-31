44c843530680103ddc3401a9977122def1c58ca3
dcheley <danielcheley@hotmail.com>
2017-08-31 17:50:05 -0400

    Separating js logic & using turbolinks script tag & precompiling assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,24 +3,3 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
-window.onload = function() {
-  document.getElementById('entry').oninput = function() {
-    var entry = document.getElementById('newEntry').value;
-    var entry = parseFloat(entry);
-
-    if (entry % 2 != 0) {
-      entry = entry * 2.50 + 0.5;
-    } else {
-      entry = entry * 2.5;
-    }
-    finalPrice = currencyFormat(entry);
-    document.getElementById('prices').innerHTML = '<p>' + finalPrice + '</p>';
-
-    function currencyFormat(number) {
-      var finalPrice = parseFloat(number);
-      finalPrice = finalPrice.toFixed(2);
-      finalPrice = '$' + finalPrice;
-      return finalPrice;
-    }
-  }
-}
---
+++ public/assets/price-c29c57aa99af98468355828babec88f2d59ee509ecbec5035a5161932a0bf569.js
@@ -0,0 +1,20 @@
+document.getElementById('entry').oninput = function() {
+  var entry = document.getElementById('newEntry').value;
+  var entry = parseFloat(entry);
+
+  if (entry % 2 != 0) {
+    entry = entry * 2.50 + 0.5;
+  } else {
+    entry = entry * 2.5;
+  }
+  finalPrice = currencyFormat(entry);
+  document.getElementById('prices').innerHTML = '<p>' + finalPrice + '</p>';
+
+  function currencyFormat(number) {
+    var finalPrice = parseFloat(number);
+    finalPrice = finalPrice.toFixed(2);
+    finalPrice = '$' + finalPrice;
+    return finalPrice;
+  }
+}
+;
